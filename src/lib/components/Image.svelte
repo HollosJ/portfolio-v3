f0d69fcf02cdfbf8941ee31deba9d323db21fe38
HollosJ <hollosj99@gmail.com>
2023-08-02 21:20:32 +0100

    Image component with

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from 'svelte';
+  export let src, alt, width, height;
+
+  let loaded = false;
+
+  onMount(() => {
+    const img = new Image();
+    img.src = src;
+
+    img.onload = () => {
+      loaded = true;
+    };
+  });
+</script>
+
+{#if loaded}
+  <img {src} {alt} {width} {height} class={`${$$props.class}`} />
+{:else}
+  <div class="w-full h-full bg-gray-300 animate-pulse" />
+{/if}
